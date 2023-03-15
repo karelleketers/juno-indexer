b5004173bbb4020bc647c3399a25bf83addaf497
karelleketers <karekete@student.arteveldehs.be>
2023-03-15 16:52:35 +0100

    handle Tokens and Transfer Events

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,33 +1,65 @@
-import { Message } from "../types";
-import { CosmosMessage, CosmosEvent } from "@subql/types-cosmos";
+import { Token, TransferEvent } from "../types";
+import { CosmosEvent } from "@subql/types-cosmos";
 import { CODE_ID } from "../utils";
 
 //Only allow relevant codeId(s) to pass through
-const isCorrectCodeId = (msg: CosmosMessage) => {
-  return (msg.msg.decodedMsg.codeId.low && msg.msg.decodedMsg.codeId.low in CODE_ID)
+const isCorrectCodeId = (ev: CosmosEvent) => {
+  return (ev.msg.msg.decodedMsg.codeId.low && ev.msg.msg.decodedMsg.codeId.low in CODE_ID)
 }
 
-//save data to Message entity in db
-export const handleMessage = async (msg: CosmosMessage): Promise<void> => {
-  if (!isCorrectCodeId(msg)) return;
-  const messageRecord = Message.create({
-    id: `${msg.tx.block.block.id}-${msg.tx.idx}`,
-    blockHeight: BigInt(msg.tx.block.block.header.height),
-    txHash: msg.tx.hash,
-    name: msg.msg.decodedMsg.msg.name,
-    symbol: msg.msg.decodedMsg.msg.symbol,
-    decimals: msg.msg.decodedMsg.msg.decimals,
+const verifyToken = async (address: string): Promise<Token | undefined> => {
+  try { const token = await Token.getByAddress(address);
+  if (!token) return;
+  token.transferEventCount += 1;
+  await token.save();
+  return token;
+  } catch(error) {
+    logger.error
+  }
+}
+
+//save data to Token entity in db
+export const handleInstantiateEvent = async (event: CosmosEvent): Promise<void> => {
+  try { 
+  if (!isCorrectCodeId(event)) return;
+  const contractAddress = event.event.type === "instantiate" && event.event.attributes.find(attribute => attribute.key === "_contract_address");
+
+  const tokenRecord = Token.create({
+      id: `${event.tx.block.block.id}-${event.tx.idx}`,
+      address: contractAddress.value,
+      decimals: event.msg.msg.decodedMsg.msg.decimals,
+      name: event.msg.msg.decodedMsg.msg.name,
+      symbol: event.msg.msg.decodedMsg.msg.symbol,
+      source: event.msg.msg.decodedMsg.sender,
+      transferEventCount: 0,
+      totalSupply: "0",
+      totalTransferred: "0"
   });
-  await messageRecord.save();
+  await tokenRecord.save();
+  } catch (error) {
+    logger.error
+  }
 }
 
-export const handleEvent = async (event: CosmosEvent): Promise<void> => {
-  /* const record = new EventEntity(
-    `${event.tx.tx.txhash}-${event.msg.idx}-${event.idx}`
-  );
-  record.blockHeight = BigInt(event.block.block.block.header.height);
-  record.txHash = event.tx.tx.txhash;
-  await record.save(); */
-  /* logger.info(JSON.stringify(event)); */
-  logger.info("there is info here")
+//try catch
+export const handleExecuteEvent = async (event: CosmosEvent): Promise<void> => {
+  try { 
+    const address = event.msg.msg.decodedMsg.contract ?? "";
+    const token = await verifyToken(address);
+    if (!token) return;
+    const transferRecord = TransferEvent.create({
+      id: `${event.tx.block.block.id}-${event.tx.idx}`,
+      //@ts-ignore
+      token: token,
+      amount: event.msg.msg.decodedMsg.msg.transfer.amount,
+      sender: event.msg.msg.decodedMsg.sender,
+      destination: event.msg.msg.decodedMsg.msg.transfer.recipient,
+      block: event.msg.block.block.header.height,
+      timestamp: event.msg.block.block.header.time,
+      transaction: event.msg.tx.hash,
+    });
+    await transferRecord.save();
+  } catch(error) {
+    logger.error
+  }
 }
